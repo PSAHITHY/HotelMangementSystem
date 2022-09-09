4e02c200fb168954287a8c0ebdd228c0b0ad670d
sahithya <sahithya.parnapalli@capgemini.com>
2022-09-09 16:24:10 +0530

    Hotel

---
+++ hotelmanagement/src/Components/AboutUs.js
@@ -0,0 +1,10 @@
+import React from "react";
+function AboutUs(){
+return(
+    <div className="aboutus">
+        <p>This Hotel was established in the year 2000.It was top most hotel in the city Hyderabad.</p>
+        <p>Owner of the hotel is Sahithya</p>
+    </div>
+)
+}
+export default AboutUs;
